2c8f06e6f5512659d2c35d4953fb4b41dd0ba4dd
polyu <a276201510@gmail.com>
2013-04-16 14:45:39 +0800

    Update config.inc.php

--- gallery/config.inc.php
+++ gallery/config.inc.php
@@ -3,12 +3,5 @@ include_once 'phpFlickr.php';
 
 class Config
 {
-static $FLICKRKEY="1443e7b2e52012a4e76a4a62c1e12bb6";
-static $FLICKRSEC="b7e001245ccbf892";
-static $FILCKRTOKEN="72157633214986343-7b5e06224445b51a";
-static $MAILOFYING="498089747@qq.com";
-static $MAILOFWEN="276201510@qq.com";
-static $MAILNAME="桂英的马仔";
-static $WEBSITEADDRESS="http://hellowind.tk/gallery/";
-static $LOCALMAIL="webmaster@hellowind.tk";
+
 }
